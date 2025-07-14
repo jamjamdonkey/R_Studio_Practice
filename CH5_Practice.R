30107127700610841c7973002635338848261cdf
JamJamDonkey <162420689+jamjamdonkey@users.noreply.github.com>
2025-07-15 05:08:39 +0900

    Add files via upload

--- CH5_Practice.R
+++ CH5_Practice.R
@@ -126,3 +126,49 @@ View (bind_col_inner)
 # 키 변수를 기준으로 모두 가로 결합하기
 bind_col_full <- full_join(jeju_y21_history, jeju_y20_history, by = "ID")
 View(bind_col_full)
+
+# reshape2 패키지 설치하기
+install.packages("reshape2")
+
+# airquality 데이터 세트 확인하기
+head(airquality)
+
+# 변수명 소문자로 통일하기
+names(airquality) <- tolower(names(airquality))
+head(airquality)
+
+# 열을 행으로 바꾸기
+library(reshape2)
+melt_test <- melt(airquality)
+head(melt_test)
+
+tail(melt_test)
+
+# 기준을 정해 열을 행으로 바꾸기
+melt_test2 <- melt(airquality, id.vars = c("month", "wind"),
+                   measure.vars = "ozone")
+head(melt_test2)
+
+# 변수명 소문자로 통일하기
+names(airquality) <- tolower(names(airquality))
+head(airquality)
+
+# 열을 행으로 바꾸기
+library(reshape2)
+aq_melt <- melt(airquality, id.vars = c("month", "day"), na.rm = TRUE)
+head(aq_melt)
+
+# 행을 열로 바꾸기
+aq_dcast <- dcast(aq_melt, month + day ~ variable)
+head(aq_dcast)
+
+view(airquality)
+view(aq_dcast)
+view(aq_melt)
+
+acast(aq_melt, day ~ month ~ variable)
+
+acast(aq_melt, month ~ variable, mean)
+
+dcast(aq_melt, month ~ variable, sum)
+
